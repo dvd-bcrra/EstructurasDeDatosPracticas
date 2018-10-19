3750ed0ca86359d5807f10b27a737e6fb62bbb75
dvd-bcrra <36464063+dvd-bcrra@users.noreply.github.com>
2018-10-19 13:24:17 -0700

    Create actividad18.java

---
+++ actividad18.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class LinkedList {
+
+    class Node{
+        String data;
+        Node next;
+    }
+
+    private Node root;
+
+    LinkedList(){
+        root = null;
+    }
+
+    private void Ingresar(String item){
+        Node nuevo = new Node();
+        nuevo.data = item;
+        //SI LA LISTA ESTA VACIA...
+        if(root == null){
+            root = nuevo;
+        }else{
+            Node reco = root;
+            while (reco.next != null){
+                reco = reco.next;
+            }
+            reco.next = nuevo;
+        }
+    }
+
+    private void Imprimir(){
+        Node reco = root;
+        while (reco != null){
+            System.out.println(reco.data);
+            reco = reco.next;
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        LinkedList ll = new LinkedList();
+        ll.Ingresar("uno");
+        ll.Ingresar("dos");
+        ll.Ingresar("tres");
+        ll.Ingresar("cuatro");
+        ll.Imprimir();
+    }
+}
